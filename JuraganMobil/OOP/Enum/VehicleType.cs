840aeeffaa58240417a1099543cba960b232376a
alvan <alvan.ghon@gmail.com>
2023-01-19 11:18:40 +0700

    add Model with Factory Pattern

---
+++ JuraganMobil/OOP/Enum/VehicleType.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JuraganMobil.OOP.Enum
+{
+    internal enum VehicleType
+    {
+        ALL = 0,
+        SUV = 1,
+        Taxi = 2,
+        PrivateJet =3
+    }
+}
---
+++ JuraganMobil/OOP/Model/BaseVehicle.cs
@@ -0,0 +1,17 @@
+﻿using JuraganMobil.OOP.Enum;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JuraganMobil.OOP.Model
+{
+    internal class BaseVehicle
+    {
+        public string? NoPoliceReg { get; set; }
+        public VehicleType VehicleTypeType { get; set; }
+        public string Year { get; set; }
+        public double price { get; set; }
+    }
+}
