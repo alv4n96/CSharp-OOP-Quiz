3b5cb4b96763f2e59cb3e493f39c2aa8bd06cd9a
alvan <alvan.ghon@gmail.com>
2023-01-19 11:29:41 +0700

    Implementation Struct and Record for hold Value

---
+++ JuraganMobil/OOP/Struct/VehicleRecord.cs
@@ -0,0 +1,18 @@
+﻿using JuraganMobil.OOP.Enum;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JuraganMobil.OOP.Struct
+{
+    internal record VehicleRecord
+    {
+        public string NoPoliceReg { get; set; }
+        public VehicleType? VehicleType { get; set; }
+        public DateTime TransactionDate { get; set; }
+        public double Rent { get; set; }
+        public double DriverFree { get; set; }
+    }
+}
